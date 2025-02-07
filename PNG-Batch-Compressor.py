578eca80bd848cb6efe9a9648d629425f79ddfe3
ZexWoo <zexwoo@qq.com>
2025-02-07 22:05:30 +0800

    first update

---
+++ PNG-Batch-Compressor.py
@@ -0,0 +1,113 @@
+import os
+
+def findext(dir, exts):
+    """输入目录和所需拓展名，返回目录和子目录中所有符合条件的文件名列表。"""
+    allfile = []
+    for path_name, folder_names, files in os.walk(dir):
+        for file in files:
+            allfile.append(os.path.join(path_name, file))
+    # 返回过滤器对象
+    files_filter = filter(lambda x: os.path.splitext(x)[1] in exts, allfile)
+    file_list = list(files_filter)
+    return file_list
+
+def optimization_selector():
+    """选择压缩力度。"""
+    number = input("选择压缩力度，越大则体积越小，速度越慢（0~7）：")
+    match number:
+        case "0":
+            optimization = '-o 0 '
+        case "1":
+            optimization = '-o 1 '
+        case "2":
+            optimization = '-o 2 '
+        case "3":
+            optimization = '-o 3 '
+        case "4":
+            optimization = '-o 4 '
+        case "5":
+            optimization = '-o 5 '
+        case "6":
+            optimization = '-o 6 '
+        case "7":
+            optimization = '-o max '
+        case _:
+            optimization = ''
+    return optimization
+
+def interlacing_selector():
+    """设置是否使用交错，交错会增大体积。"""
+    number = input("是否使用交错（0=去除交错，1=使用交错，回车保持原样）：")
+    match number:
+        case "0":
+            interlacing = '-i 0 '
+        case "1":
+            interlacing = '-i 1 '
+        case _:
+            interlacing = '-i keep'
+    return interlacing
+
+def strip_selector():
+    """选择元数据的去除方案，去除无关元数据可以减小体积。"""
+    number = input("""选择元数据的去除方案：
+0=不去除
+1=仅去除不影响图片渲染的元数据
+2=去除所有对图片不重要的元数据
+你的选择是：""")
+    match number:
+        case "1":
+            strip = '--strip safe'
+        case "2":
+            strip = '--strip all'
+        case _:
+            strip = ''
+    return strip
+
+def param_selector():
+    """设置压缩参数。"""
+    param = ''
+    auto = input("回车以使用 oxipng 默认设置，输入 1 手动设置一些选项：")
+    if auto:
+        optimization = optimization_selector()
+        interlacing = interlacing_selector()
+        strip = strip_selector()
+
+        param += optimization + interlacing + strip
+    else:
+        param = ''
+
+    return param
+
+if __name__ == '__main__':
+    # 获取当前目录
+    current_path = os.getcwd()
+    # 确认输入目录是否存在，如不存在，则创建
+    input_path = current_path + '\\Input'
+    if not os.path.exists(input_path):
+        os.mkdir('./Input')
+        print("「Input」文件夹不存在，已帮你创建好！")
+    # 确认输出目录是否存在，如不存在，则创建
+    output_path = current_path + '\\Output'
+    if not os.path.exists(output_path):
+        os.mkdir('./Output')
+        print("「Output」文件夹不存在，已帮你创建好！")
+
+    # 支持的输入文件扩展名
+    exts = ['.png', '.PNG']
+    # 获得待处理的文件列表
+    files = findext(input_path, exts)
+    if files:
+        param = param_selector()
+
+        for path in files:
+            # 调用 oxipng
+            if param:
+                command = f"oxipng {param} --dir=\"{output_path}\\{param}\" \"{path}\""
+            else:
+                command = f"oxipng {param} --dir=\"{output_path}\\default param\" \"{path}\""
+            print(f"使用的命令为：\n\n{command}\n")
+            os.system(command)
+            print(f"\n当前进度：{files.index(path) + 1}/{len(files)}\n{'-' * 30}")
+        print("所有压缩任务已完成！")
+    else:
+        print("待压缩 PNG 目录为空！请将需要压缩的 PNG 都存放到 Input 文件夹中！")
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # PNG-Batch-Compressor
+
 能够批量且自定义参数的 PNG 批量压缩器。
+
+## 用法
+
+### 准备好 oxipng
+
+首先，你需要下载 oxipng 这款压缩器。
+
+[点此](https://github.com/shssoichiro/oxipng/releases) 下载适合你设备的版本。
+
+下载后，解压到你觉得便于管理的目录。
+
+将 `oxipng-9.1.3-x86_64-pc-windows-msvc` 文件夹所在的目录加入系统的 Path 环境变量：
+
+1. 右键单击「开始」按钮。
+2. 单击「系统」。
+3. 单击「高级系统设置」。
+4. 单击「环境变量…」。
+5. 在「系统变量」一栏，选中「Path」所在的一栏。
+6. 单击「编辑…」。
+7. 单击「新建」。
+8. 将 `oxipng-9.1.3-x86_64-pc-windows-msvc` 文件夹所在的目录粘贴到新增的这一栏。
+9. 单击「确定」三次，关掉所有窗口。
+
+确认 oxipng 已能成功运作：
+
+1. 右键单击「开始」按钮。
+2. 单击「终端」。
+3. 输入 `oxipng`，然后回车。
+4. 如果展示下列信息，则说明成功。
+
+```
+error: the following required arguments were not provided:
+  <files>...
+
+Usage: oxipng.exe <files>...
+
+For more information, try '--help'.
+```
+
+### 开始压缩图片
+
+将 `PNG-Batch-Compressor.py` 复制粘贴到你计划压缩 PNG 的目录，运行它。
+
+它会创建 `Input` 和 `Output` 两个文件夹。
+
+将待压缩的所有 PNG 都拖入 `Input` 文件夹中。
+
+再次运行 `PNG-Batch-Compressor.py`，在每一步按提示操作，输入符合你需求的值；抑或是简单地摁下「回车」按钮使用默认参数压缩。
+
+压缩完毕的文件会出现在 `Output` 文件夹中以对应压缩参数命名的子文件夹中。
